b86afb861b6c81d9801eb956e7b2f7a98015c7e9
Carlo de Carolis <carlodeca@gmail.com>
2017-02-09 11:50:39 +0100

    Create increaser.java

---
+++ increaser.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/* This program increase the value of a number entered from the keyboard (of +1 up to +10)
+*  and print at screen all individual increments. */
+
+public class Increaser {
+	public static void main(String[] args) {
+		int value;
+		int valueIncreased;
+		
+		System.out.println("Enter the value to be incremented:");
+		
+		@SuppressWarnings("resource")
+		Scanner keyboard = new Scanner(System.in);
+		valore = keyboard.nextInt();
+		
+		System.out.println("The value added to increase is: " + value);
+		System.out.println("Here are the increased series:");
+		
+		valueIncreased = value + 10;
+		
+		while (value <= valueIncreased) {
+			System.out.println(value++);
+		}
+	}
+}
